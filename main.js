2fa8d0ddb9f3789ea3da25d4d9f0fba9fa4a7327
Mihai <mihai@192-168-0-136.rdsnet.ro>
2024-02-19 09:42:00 +0200

    added the Characters class and the eject and display functions

---
+++ main.js
@@ -0,0 +1,59 @@
+console.log('main.js loaded!');
+
+class Characters{
+    constructor(name, color, avatar){
+        this.name = name;
+        this.color = color;
+        this.avatar = avatar;
+        this.isImpostor = Math.random() < 0.3;
+        this.isAlive = true;
+    }
+
+    eject(){
+        if (this.isAlive == true) {
+            this.isAlive = false;
+            
+            let deathMsg = this.isImpostor 
+            ? console.log(`${this.name} was ejected. He was an Impostor`) 
+            : console.log(`${this.name} was ejected. He was not an Impostor`) ;
+        }
+        else{
+            console.log(`Can't eject ${this.name}. He is already dead!`);
+        }
+    }
+
+    display(){
+        if (this.isAlive == true) {
+            console.log(`${this.name}${this.avatar} (${this.color}). Status: Alive and Well 😇`);
+        }
+        else{
+           this.isImpostor 
+           ? console.log(`${this.name}${this.avatar} (${this.color}). Status: Deceased 💀 - Impostor`)
+           : console.log(`${this.name}${this.avatar} (${this.color}). Status: Deceased 💀 - Not an Impostor`);
+        }
+
+    }
+}
+
+let players = [
+    new Characters('Marian Lopataru', 'brown', '⛏'),
+    new Characters('Vlad Tepes', 'red', '🧛🏻‍♂️'),
+    new Characters('Andrei Gucci', 'yellow', '💵'),
+    new Characters('Dorian Popa', 'green', '🍃'),
+    new Characters('Catalin Scarlatescu', 'orange', '🍔'),
+    new Characters('Nicolae Ceausescu', 'grey', '🧙‍♂️'),
+    new Characters('Osama bin Laden', 'black', '💣')
+];
+
+let impostors = players.filter(player => player.isImpostor);
+
+function displayPlayers(){
+    for (let i = 0; i < players.length; i++) {
+        players[i].display();
+    }
+}
+
+function ejectPlayer(n){
+    players[n].eject();
+    players[n].display();
+}
